c673432a67e4f95da0714767c473dbb0608f0b11
AbaevB <abaev70@hotmail.com>
2022-08-19 22:49:40 +0700

    fix

--- Layout/js/main.js
+++ Layout/js/main.js
@@ -1,20 +1,30 @@
-$(function(){
+$(function () {
+  $(function () {
 
-	//scroll to top
-	$('.button-up').click(function(){
-		$('html, body').animate({ scrollTop: 0 }, 300);
-		return false;
-	});
+    $(window).on('load', function () {
+      $('.preloader').delay(500).fadeOut('slow', function () {
+        $(this).attr('style', 'display: none !important');
+      });
+
+    });
+    $(function () {
+
+      //scroll to top
+      $('.button-up').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 300);
+        return false;
+      });
 
 
-	//show up-button
-	$(document).scroll(function(){
-		var y = $(this).scrollTop();
-		if (y > 800) {
-			$('.button-up').removeClass('hidden');
-		} else {
-			$('.button-up').addClass('hidden');
-		}
-	});
-
+      //show up-button
+      $(document).scroll(function () {
+        var y = $(this).scrollTop();
+        if (y > 800) {
+          $('.button-up').removeClass('hidden');
+        } else {
+          $('.button-up').addClass('hidden');
+        }
+      });
+    });
+  });
 });
